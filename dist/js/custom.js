7fa1688ff8c6d949a6f97dde218ca6689859a566
midhun kp <mithula@adamscreation.com>
2021-03-22 19:46:33 +0530

    new pages added

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -127,3 +127,31 @@ if (prdct_slider != null) {
 
 
 */
+/**tab */
+
+
+$(document).ready(function() {
+    var previousActiveTabIndex = 0;
+
+    $(".tab-switcher").on('click keypress', function(event) {
+        // event.which === 13 means the "Enter" key is pressed
+
+        if ((event.type === "keypress" && event.which === 13) || event.type === "click") {
+
+            var tabClicked = $(this).data("tab-index");
+
+            if (tabClicked != previousActiveTabIndex) {
+                $("#allTabsContainer .tab-container").each(function() {
+                    if ($(this).data("tab-index") == tabClicked) {
+                        $(".tab-container").hide();
+                        $(this).show();
+                        previousActiveTabIndex = $(this).data("tab-index");
+                        return;
+                    }
+                });
+            }
+        }
+    });
+});
+
+/**tab */
